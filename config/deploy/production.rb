3e87fb20218cb10359afeef2f7b2c1c2981ed8e7
Dan Brubaker Horst <dan.brubaker.horst@gmail.com>
2015-02-02 10:05:57 -0500

    Making the solr config linked in prod
    Former-commit-id: b1be2565e76d22736a462dd585eabc4575bec127

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -21,7 +21,7 @@ set :domain,    fetch(:host, 'medievalprod-vm.library.nd.edu')
 set :bundle_without, [:development, :test, :debug]
 set :shared_directories,  %w(log)
 set :shared_files, %w()
-set :linked_files, ['config/database.yml']
+set :linked_files, ['config/database.yml', 'config/solr.yml']
 server fetch(:domain), user: fetch(:user), roles: %w{web app db}
 after 'deploy', 'deploy:precompile_assets'
 after 'deploy', 'deploy:db_migrate'
