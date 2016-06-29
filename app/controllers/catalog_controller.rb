5347c95fa36bf756aa7bccb5f1c58c6fd381e4a9
Dan Brubaker Horst <dan.brubaker.horst@gmail.com>
2016-06-29 15:36:43 -0400

    Adding all fields to default_solr_params
    Also whitespace cleanup
    
    
    Former-commit-id: 68d65e3b1c376958d08ccbe8950a3c516e7ea51e

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -1,7 +1,7 @@
 # -*- encoding : utf-8 -*-
 require 'blacklight/catalog'
 
-class CatalogController < ApplicationController  
+class CatalogController < ApplicationController
 
   include Blacklight::Catalog
 
@@ -24,38 +24,50 @@ class CatalogController < ApplicationController
 
   configure_blacklight do |config|
     ## Default parameters to send to solr for all search-like requests. See also SolrHelper#solr_search_params
-    config.default_solr_params = { 
+    config.default_solr_params = {
       :qt => 'search',
       :qf => [
-        'title_t',
+        'format',                     # Shared fields
+        'title_s',
         'library_facet',
-        'language_facet' 'city_facet',
-        'alternate_title_t',
-        'shelf_mark_t',
-        'origin_t',
+        'city_facet',
+        'shelf_mark_s',
+        'alternate_names_s',          # Facsimile specific fields
+        'origin_s',
         'country_of_origin_facet',
-        'century_keywords_t',
-        'date_description_t',
+        'dimensions_s',
+        'century_keywords_s',
+        'date_description_s',
         'date_range_facet',
-        'author_t',
-        'content_t',
+        'language_facet',
+        'author_s',
+        'content_s',
+        'hand_s',
         'illuminations_facet',
-        'nature_of_facsimile_t',
-        'classification_facet',
-        'bibliography_t',
-        'notes_t',
-        'publisher_t',
-        'place_of_publication_t',
-        'commentary_volume_facet',
-        'call_number_t',
-        'type_of_muiscal_notation_t',
+        'type_of_decoration_s',
         'musical_notation_facet',
-        'type_of_decoration_t'
+        'type_of_musical_notation_s',
+        'call_number_s',
+        'commentary_volume_facet',
+        'nature_of_facsimile_s',
+        'series_s',
+        'publication_date_s',
+        'place_of_publication_s',
+        'publisher_s',
+        'printer_s',
+        'notes_s',
+        'bibliography_s',
+        'classification_facet',
+        'mss_name_s',                 # Microfilm specific fields
+        'mss_note_s',
+        'collection_facet',
+        'reel_s',
+        'hesburgh_location_s',
       ],
       :rows => 10 
     }
 
-    ## Default parameters to send on single-document requests to Solr. These settings are the Blackligt defaults (see SolrHelper#solr_doc_params) or 
+    ## Default parameters to send on single-document requests to Solr. These settings are the Blackligt defaults (see SolrHelper#solr_doc_params) or
     ## parameters included in the Blacklight-jetty document requestHandler.
     #
     #config.default_document_solr_params = {
@@ -63,7 +75,7 @@ class CatalogController < ApplicationController
     #  ## These are hard-coded in the blacklight 'document' requestHandler
     #  # :fl => '*',
     #  # :rows => 1
-    #  # :q => '{!raw f=id v=$id}' 
+    #  # :q => '{!raw f=id v=$id}'
     #}
 
     # solr field configuration for search results/index views
@@ -82,17 +94,17 @@ class CatalogController < ApplicationController
     # * If left unset, then all facet values returned by solr will be displayed.
     # * If set to an integer, then "f.somefield.facet.limit" will be added to
     # solr request, with actual solr request being +1 your configured limit --
-    # you configure the number of items you actually want _ted_ in a page.    
+    # you configure the number of items you actually want _ted_ in a page.
     # * If set to 'true', then no additional parameters will be sent to solr,
     # but any 'sniffed' request limit parameters will be used for paging, with
-    # paging at requested limit -1. Can sniff from facet.limit or 
+    # paging at requested limit -1. Can sniff from facet.limit or
     # f.specific_field.facet.limit solr request params. This 'true' config
     # can be used if you set limits in :default_solr_params, or as defaults
     # on the solr side in the request handler itself. Request handler defaults
     # sniffing requires solr requests to be made with "echoParams=all", for
-    # app code to actually have it echo'd back to see it.  
+    # app code to actually have it echo'd back to see it.
     #
-    # :show may be set to false if you don't want the facet to be drawn in the 
+    # :show may be set to false if you don't want the facet to be drawn in the
     # facet bar
     config.add_facet_field 'format', :label => 'Format'
     config.add_facet_field 'library_facet', :label => 'Library', :limit => 10
@@ -120,9 +132,9 @@ class CatalogController < ApplicationController
     config.add_facet_fields_to_solr_request!
 
     # solr fields to be displayed in the index (search results) view
-    #   The ordering of the field names is the order of the display 
+    #   The ordering of the field names is the order of the display
     #config.add_index_field 'format', :label => 'Format'
-    config.add_index_field 'title_s', :label => 'Title:' 
+    config.add_index_field 'title_s', :label => 'Title:'
     config.add_index_field 'library_facet', :label => 'Library'
     config.add_index_field 'city_facet', :label => 'City:'
     config.add_index_field 'shelf_mark_s', :label => 'Shelf Mark:'
@@ -139,8 +151,8 @@ class CatalogController < ApplicationController
     config.add_index_field 'commentary_volume_facet', :label => 'Commentary Volume:'
 
     # solr fields to be displayed in the show (single result) view
-    #   The ordering of the field names is the order of the display 
-    config.add_show_field 'title_s', :label => 'Title:' 
+    #   The ordering of the field names is the order of the display
+    config.add_show_field 'title_s', :label => 'Title:'
     config.add_show_field 'library_facet', :label => 'Library:'
     config.add_show_field 'language_facet', :label => 'Language:'
     config.add_show_field 'city_facet', :label => 'City:'
@@ -186,48 +198,47 @@ class CatalogController < ApplicationController
     # The :key is what will be used to identify this BL search field internally,
     # as well as in URLs -- so changing it after deployment may break bookmarked
     # urls.  A display label will be automatically calculated from the :key,
-    # or can be specified manually to be different. 
+    # or can be specified manually to be different.
 
     # This one uses all the defaults set by the solr request handler. Which
     # solr request handler? The one set in config[:default_solr_parameters][:qt],
-    # since we aren't specifying it otherwise. 
-    
+    # since we aren't specifying it otherwise.
+
     config.add_search_field 'all_fields', :label => 'All Fields'
-    
 
     # Now we see how to over-ride Solr request handler defaults, in this
     # case for a BL "search field", which is really a dismax aggregate
-    # of Solr search fields. 
-    
+    # of Solr search fields.
+
     config.add_search_field('title') do |field|
-      # solr_parameters hash are sent to Solr as ordinary url query params. 
+      # solr_parameters hash are sent to Solr as ordinary url query params.
       field.solr_parameters = { :'spellcheck.dictionary' => 'title' }
 
       # :solr_local_parameters will be sent using Solr LocalParams
       # syntax, as eg {! qf=$title_qf }. This is neccesary to use
       # Solr parameter de-referencing like $title_qf.
       # See: http://wiki.apache.org/solr/LocalParams
-      field.solr_local_parameters = { 
+      field.solr_local_parameters = {
         :qf => '$title_qf',
         :pf => '$title_pf'
       }
     end
-    
+
     config.add_search_field('author') do |field|
       field.solr_parameters = { :'spellcheck.dictionary' => 'author' }
-      field.solr_local_parameters = { 
+      field.solr_local_parameters = {
         :qf => '$author_qf',
         :pf => '$author_pf'
       }
     end
-    
+
     # Specifying a :qt only to show it's possible, and so our internal automated
-    # tests can test it. In this case it's the same as 
-    # config[:default_solr_parameters][:qt], so isn't actually neccesary. 
+    # tests can test it. In this case it's the same as
+    # config[:default_solr_parameters][:qt], so isn't actually neccesary.
     config.add_search_field('subject') do |field|
       field.solr_parameters = { :'spellcheck.dictionary' => 'subject' }
       field.qt = 'search'
-      field.solr_local_parameters = { 
+      field.solr_local_parameters = {
         :qf => '$subject_qf',
         :pf => '$subject_pf'
       }
@@ -251,5 +262,4 @@ class CatalogController < ApplicationController
     params[:active_branch_code] = 'medieval_library'
   end
 
-
-end 
+end
