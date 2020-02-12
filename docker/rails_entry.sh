76b5ca09ca470163433f453a0428f02be1e6309b
ialford <ialford@nd.edu>
2020-02-12 09:31:53 -0500

    update rails-entry for development

--- docker/rails_entry.sh
+++ docker/rails_entry.sh
@@ -4,10 +4,13 @@
 cp /bundle/Gemfile.lock ./
 chmod a+x ./docker/wait-for-it.sh
 bash docker/wait-for-it.sh mysql:3306
-bundle exec rake db:create db:schema:load
+# bundle exec rake db:create db:schema:load
 # If we find people are ok with just starting a shell in the container and running the db create/load before running specs, then we can remove this
 # RAILS_ENV=development bundle exec rake db:create db:schema:load
-bundle exec rake jetty:clean jetty:configure
-bundle exec rake solr:index:all
+# Precompile Assets
+bundle exec rake RAILS_ENV=development assets:precompile
+#bundle exec rake RAILS_ENV=development db:migrate
+#bundle exec rake RAILS_ENV=development jetty:clean jetty:configure
+bundle exec rake RAILS_ENV=development solr:index:all
 
-exec bundle exec rails s # -p 3000 # --ssl --ssl-key-file dev_server_keys/server.key --ssl-cert-file dev_server_keys/server.crt
+exec bundle exec rails s -e development -b 0.0.0.0 # -p 3000 # --ssl --ssl-key-file dev_server_keys/server.key --ssl-cert-file dev_server_keys/server.crt
